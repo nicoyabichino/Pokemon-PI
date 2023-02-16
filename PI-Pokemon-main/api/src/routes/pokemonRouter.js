20c0ec1dab4c92db0dd87ad7d8e740b3b6210f27
nicoyabichino <nicoyabichino@gmail.com>
2023-02-16 18:40:29 -0300

    NM

--- PI-Pokemon-main/api/src/routes/pokemonRouter.js
+++ PI-Pokemon-main/api/src/routes/pokemonRouter.js
@@ -3,13 +3,16 @@ const {getPokeHandler, getPokesHandler, createPokeHandler} = require("../handler
 const pokemonRouter = Router();
 
 const validatePost = (req, res, next) => {
-const {name, hp, attack, defense, speed, types} = req.body;
+const {name, hp, attack, defense, speed, types, image, height, weight} = req.body;
 if(!name) {return res.status(404).send({error: "Falta agregar el nombre del Pokemon"})}
 else if(!hp) {return res.status(404).send({error: "Falta agregar la vida maxima del Pokemon"})}
 else if(!attack) {return res.status(404).send({error: "Falta agregar el poder de ataque del Pokemon"})}
 else if(!defense) {return res.status(404).send({error: "Falta agregar el nivel de defensa del Pokemon"})}
 else if(!speed) {return res.status(404).send({error: "Falta agregar la velocidad del Pokemon"})}
 else if(!types) {return res.status(404).send({error: "Falta agregar el tipo de Pokemon"})}
+else if(!image) {return res.status(404).send({error: "Falta agregar una imagen del Pokemon"})}
+else if(!height) {return res.status(404).send({error: "Falta agregar el peso del Pokemon"})}
+else if(!weight) {return res.status(404).send({error: "Falta agregar la altura de Pokemon"})}
    else {
        next();
    }
